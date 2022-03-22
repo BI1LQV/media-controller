5e03562cf8caad6804e32a6e61b19628ace72fe4
yuanjiahao02 <yuanjiahao02@meituan.com>
2022-03-22 16:10:24 +0800

    联合scanner和train

--- helper.py
+++ helper.py
@@ -1,3 +1,4 @@
+from re import I
 from numpy import Inf, Infinity
 
 
@@ -15,9 +16,9 @@ def preProcess(list):
         for (x,y,z) in img:
             xmax=max(x,xmax)
             xmin=min(x,xmin)
-            ymax=min(y,ymax)
+            ymax=max(y,ymax)
             ymin=min(y,ymin)
-            zmax=min(z,zmax)
+            zmax=max(z,zmax)
             zmin=min(z,zmin)
         for line in img:
             line[0]=(line[0]-xmin)/(xmax-xmin)
@@ -25,3 +26,12 @@ def preProcess(list):
             line[2]=(line[2]-zmin)/(zmax-zmin)
         res.append(img)
     return res
+
+def maxIndex(list):
+    weightMax=-1
+    index=-1
+    for i in range(len(list)):
+        if list[i]>weightMax:
+            weightMax=list[i]
+            index=i
+    return index
