8fe3c6526e00e23ce1674f3f0832ec0d106a3747
yuanjiahao02 <yuanjiahao02@meituan.com>
2022-03-22 18:50:40 +0800

    feat:执行器增加执行间隔

--- executer.py
+++ executer.py
@@ -13,4 +13,18 @@ indexToActionMap = [
 
 
 def actionExecute(i):
-    indexToActionMap[i]()
+    def exec():
+        indexToActionMap[i]()
+        print(i, 'exec')
+        global lastAction
+        global lastExecTime
+        lastAction = i
+        lastExecTime = time.time()
+    if i in [0, 1, 2, 3]:  # 为持续性动作
+        if lastAction != i:  # 不同动作 直接执行
+            exec()
+        elif time.time()-lastExecTime > 0.5:  # 相同动作 间隔500毫秒执行
+            exec()
+    else:  # 为一次性动作
+        if lastAction != i:  # 必须为不同动作才能执行
+            exec()
